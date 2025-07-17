e47c276f50ae83f8d11da30e0ff5afb4c7b57cfb
Teja-Kumar <teja.kumar@coxautoinc.com>
2025-07-17 12:24:25 +0530

    Frst tf commit

--- vm.tf
+++ vm.tf
@@ -12,41 +12,41 @@ data "azurerm_subnet" "subnet" {
 }
 
 # Create a new Network Interface connected to the existing Subnet
-# resource "azurerm_network_interface" "example" {
-#   name                = "${var.prefix}-nic"
-#   location            = var.location # Update the location if necessary
-#   resource_group_name = data.azurerm_virtual_network.vnet.resource_group_name # Replace with your resource group name
+resource "azurerm_network_interface" "example" {
+  name                = "${var.prefix}-nic"
+  location            = var.location # Update the location if necessary
+  resource_group_name = data.azurerm_virtual_network.vnet.resource_group_name # Replace with your resource group name
 
-#   ip_configuration {
-#     name                          = "${var.prefix}-ip"
-#     subnet_id                     = data.azurerm_subnet.subnet.id
-#     private_ip_address_allocation = "Dynamic"
-#   }
-# }
+  ip_configuration {
+    name                          = "${var.prefix}-ip"
+    subnet_id                     = data.azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
 
-# resource "azurerm_windows_virtual_machine" "testvm" {
-#   name                  = "${var.prefix}-01"
-#   location              = var.location
-#   resource_group_name   = data.azurerm_virtual_network.vnet.resource_group_name
-#   size                  = "Standard_B2s"
-#   network_interface_ids = [azurerm_network_interface.example.id]
-#   admin_username        = "azadmin"
-#   admin_password        = "Admin@123"
+resource "azurerm_windows_virtual_machine" "testvm" {
+  name                  = "${var.prefix}-01"
+  location              = var.location
+  resource_group_name   = data.azurerm_virtual_network.vnet.resource_group_name
+  size                  = "Standard_B2s"
+  network_interface_ids = [azurerm_network_interface.example.id]
+  admin_username        = "azadmin"
+  admin_password        = "Admin@123"
 
-#   os_disk {
-#     name                 = "${var.prefix}-disk1"
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
+  os_disk {
+    name                 = "${var.prefix}-disk1"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-#   source_image_reference {
-#     publisher = "MicrosoftWindowsServer"
-#     offer     = "WindowsServer"
-#     sku       = "2019-Datacenter"
-#     version   = "latest"
-#   }
-# }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
 
 
 
