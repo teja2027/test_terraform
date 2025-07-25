417372d543f7c8a0efd3aac12f9ec2650c78c80f
Teja-Kumar <teja.kumar@coxautoinc.com>
2025-07-25 14:47:23 +0530

    aks code

--- aks.tf
+++ aks.tf
@@ -1,20 +1,7 @@
-# Reference the existing Virtual Network
-data "azurerm_virtual_network" "aksvnet" {
-  name                = "aks-vnet"
-  resource_group_name = "myRg"
-}
-
-# Reference the existing Subnet
-data "azurerm_subnet" "akssubnet" {
-  name                 = "default"
-  virtual_network_name = data.azurerm_virtual_network.aksvnet.name
-  resource_group_name  = data.azurerm_virtual_network.vnet.resource_group_name
-}
-
 resource "azurerm_kubernetes_cluster" "aks" {
   name                      = "testaks01"
   location                  = var.location # Update the location if necessary
-  resource_group_name       = data.azurerm_virtual_network.aksvnet.resource_group_name
+  resource_group_name       = data.azurerm_virtual_network.vnet.resource_group_name
   dns_prefix                = "aksdns"
   kubernetes_version        = "1.32.5"
   sku_tier                  = "Free"
@@ -23,7 +10,11 @@ resource "azurerm_kubernetes_cluster" "aks" {
     name                = "default"
     node_count          = 1
     vm_size             = "Standard_D2_v4"
-    vnet_subnet_id      = data.azurerm_subnet.akssubnet.id
+    vnet_subnet_id      = data.azurerm_subnet.subnet.id
+  }
+
+   network_profile {
+    network_plugin    = "azure"
   }
 
   identity {
